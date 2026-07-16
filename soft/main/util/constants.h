140eb537aa024708ef59ffe1fd572c8fd4b24a37
nikoloc <nikoloc2@proton.me>
2026-07-16 11:33:45 +0200

    added probe functions to the generator interface
    this way we can decouple all the generator logic from the controller

--- soft/main/util/constants.h
+++ soft/main/util/constants.h
@@ -3,11 +3,14 @@
 
 #define VDD (3.3f)
 #define MAX_OFFSET (12)
+#define MIN_OFFSET (-12)
+
+// test these values
 #define MAX_SINE_FREQ (100000)
-#define MIN_SIN_FREQ (150) // Testirati
+#define MIN_SINE_FREQ (150)
 #define MAX_RECT_FREQ (2000000)
-#define MIN_RECT_FREQ (10) // ?
+#define MIN_RECT_FREQ (10)
 #define MAX_TRI_FREQ (30000)
-#define MIN_TRI_FREQ (100) // ?
+#define MIN_TRI_FREQ (100)
 
 #endif
